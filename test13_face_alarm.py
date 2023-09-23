b32fd5b55ff0bceea3063b11e8c775906b0cb3ab
qiaoliang <qiaoliang.email@gmail.com>
2023-09-23 12:44:42 +0800

    play sound by the package pygame as soon as a face is showed

--- test13_face_alarm.py
+++ test13_face_alarm.py
@@ -1,11 +1,12 @@
 import cv2
 from cvzone.FaceMeshModule import FaceMeshDetector
-from playsound import playsound
+import pygame
+
 cap = cv2.VideoCapture(0)  # 0 for webcam, 0 for external camera
 detector = FaceMeshDetector(maxFaces=1) # maxFaces=1 means only one face is detected, it will be faster.
 
-# 初始化状态
-face_detected = False
+pygame.init()
+pygame.mixer.music.load("alert.mp3")
 
 while True:
 
@@ -18,17 +19,9 @@ while True:
     img, faces = detector.findFaceMesh(img)
     # 如果检测到人脸
     if faces:
-        # 如果之前没有检测到人脸，播放 alert.mp3
-        if not face_detected:
-            playsound("alert.mp3")
-            face_detected = True
-            sound_playing = True
+        pygame.mixer.music.play()
     else:
-        # 如果之前检测到人脸，停止播放
-        if sound_playing:
-            playsound("alert.mp3", False)  # 停止声音
-            sound_playing = False
-            face_detected = False
+        pygame.mixer.music.stop()  # 立即停止声音播放
     # 视频太大的话 可以将视频resize成目标大小
     img = cv2.resize(img, (640, 360))
     cv2.imshow('image', img)
