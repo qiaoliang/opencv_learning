f5082e65b34eb24421604c27d4b3688fb42cb2f6
qiaoliang <qiaoliang.email@gmail.com>
2023-09-24 08:55:02 +0800

    example 15: 加入一个注释.另外,示例图片中还有一个二维码检测不出来.

--- test15_qrcode.py
+++ test15_qrcode.py
@@ -13,27 +13,29 @@ def find_squares(img):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     bin = cv.Canny(gray, 30, 100, apertureSize=3)
     contours, _hierarchy = cv.findContours(bin, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cv.imshow('afterCanny',bin )
+    ch = cv.waitKey()
     print("轮廓数量：%d" % len(contours))
     index = 0
     # 轮廓遍历
     for cnt in contours:
         cnt_len = cv.arcLength(cnt, True) #计算轮廓周长
-        cnt = cv.approxPolyDP(cnt, 0.02*cnt_len, True) #多边形逼近
-        # 条件判断逼近边的数量是否为4，轮廓面积是否大于1000，检测轮廓是否为凸的
-        if len(cnt) == 4 and cv.contourArea(cnt) > 1000 and cv.isContourConvex(cnt):
-            M = cv.moments(cnt) #计算轮廓的矩
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])#轮廓重心
+        cnt = cv.approxPolyDP(cnt, 0.1*cnt_len, True) #多边形逼近, x*cnt_len为精度参数,越小越精确。0.1 可以检测出三个，0.2 时检测出零个。当为0.02 时，最终会检测出两个轮廓。
+        if len(cnt) == 4: # 条件判断逼近边的数量是否为4
+            if  cv.contourArea(cnt) > 600: # 条件判断轮廓面积是否大于1000
+               if cv.isContourConvex(cnt): # 检测轮廓是否为凸的
+                    print(cnt)
+                    M = cv.moments(cnt) #计算轮廓的矩
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])#轮廓重心
 
-            cnt = cnt.reshape(-1, 2)
-            max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
-            # 只检测矩形（cos90° = 0）
-            if max_cos < 0.1:
-            # 检测四边形（不限定角度范围）
-            #if True:
-                index = index + 1
-                cv.putText(img,("#%d"%index),(cx,cy),font,0.7,(255,0,255),2)
-                squares.append(cnt)
+                    cnt = cnt.reshape(-1, 2)
+                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
+                    # 只检测矩形（cos90° = 0）
+                    if max_cos < 0.1:
+                        index = index + 1
+                        cv.putText(img,("#%d"%index),(cx,cy),font,0.7,(255,0,255),2)
+                        squares.append(cnt)
     return squares, img
 
 img = cv.imread("./qrcode.png")
